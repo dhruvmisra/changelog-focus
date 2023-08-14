dd345462d4dbc128762eba35b4c6858009d43d80
dhruvmisra <dhruvmisra@live.com>
2023-08-14 14:34:44 +0530

    Add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -148,6 +148,13 @@ const Home: NextPage = () => {
                 <title>Changelog Focus</title>
                 <meta name="description" content="Focus on the changelog relevant to you" />
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
             </Head>
             <main className="mx-4 flex min-h-screen flex-col items-center" ref={mainRef}>
                 <div className="container flex flex-col items-center justify-center py-4 ">
