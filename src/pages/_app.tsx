2bc618a2834d94c85cee200f584cc9c6dc9c5436
dhruvmisra <dhruvmisra@live.com>
2023-08-31 00:32:07 +0530

    Add next-seo to simplify SEO config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,15 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 import { Toaster } from "react-hot-toast";
+import { DefaultSeo } from "next-seo";
 import NProgress from "nprogress";
+import { Header } from "@/components/Header";
 import { api } from "@/utils/api";
 import * as gtag from "@/utils/gtag";
+import { DEFAULT_SEO } from "@/utils/seo";
 
 import "@/styles/globals.css";
 import "@/styles//nprogress.css";
-import { Header } from "@/components/Header";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
     const router = useRouter();
@@ -34,7 +36,8 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     }, [router.events]);
 
     return (
-        <div className="container mx-auto min-h-screen px-2 sm:px-4">
+        <div className="container mx-auto min-h-screen px-4">
+            <DefaultSeo {...DEFAULT_SEO} />
             <Header />
             <Toaster position="bottom-center" />
             <Component {...pageProps} />
---
+++ src/utils/seo.ts
@@ -0,0 +1,52 @@
+export const DEFAULT_SEO = {
+    title: "Changelog Focus",
+    description: "Focus on the changelog relevant to you.",
+    openGraph: {
+        type: "website",
+        locale: "en_IE",
+        url: "https://changelogfocus.dhruvmisra.com",
+        siteName: "ChangelogFocus",
+    },
+    additionalMetaTags: [
+        {
+            name: "msapplication-TileColor",
+            content: "#00a300",
+        },
+        {
+            name: "theme-color",
+            content: "#ffffff",
+        },
+    ],
+    additionalLinkTags: [
+        {
+            rel: "icon",
+            href: "/favicon.ico",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "/favicon-32x32.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "/favicon-16x16.png",
+        },
+        {
+            rel: "apple-touch-icon",
+            href: "/apple-touch-icon.png",
+            sizes: "180x180",
+        },
+        {
+            rel: "mask-icon",
+            href: "/safari-pinned-tab.svg",
+            color: "#5bbad5",
+        },
+        {
+            rel: "manifest",
+            href: "/site.webmanifest",
+        },
+    ],
+};
